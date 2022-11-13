ecc9a7a709203129a7de4e686650add5241be8d0
Victor Mendoza <vmendoza2002@hotmail.com>
2022-11-12 21:31:11 -0600

    Create pantalla para mostrar datos

---
+++ listado/src/main/java/com/construccion/MostrarDatos.java
@@ -0,0 +1,14 @@
+package com.construccion;
+
+/**
+ * Hello world!
+ *
+ */
+public class MostrarDatos 
+{
+    public static void main( String[] args )
+    {
+        SystemApp systemApp = new SystemApp("jsonFile/", "employee.json");
+        System.out.println(systemApp.getJsonText());
+    }
+}
