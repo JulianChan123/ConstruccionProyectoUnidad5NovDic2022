14bfeb12127aaa0d58f9849d4b431fbbf2a846c7
Julian Chan Palomo <A20216390@alumnos.uady.mx>
2022-12-01 14:03:22 -0600

    Unit Test SystemApp.Search()

---
+++ listado/src/test/java/com/construccion/MostrarDatosTest.java
@@ -0,0 +1,4 @@
+package com.construccion;
+public class MostrarDatosTest {
+    
+}
--- listado/src/test/java/com/construccion/SystemAppTest.java
+++ listado/src/test/java/com/construccion/SystemAppTest.java
@@ -2,9 +2,14 @@ package com.construccion;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 class SystemAppTest {
 	
@@ -27,6 +32,18 @@ class SystemAppTest {
 	@DisplayName("Probando si es un archivo valido")
 	void shouldIsValidFile() {
 		assertTrue(systemApp.isValidFile());
+	}
+
+	@Test
+	@DisplayName("Probando si busca")
+	void shouldSearch() throws JsonMappingException, JsonProcessingException {
+		ArrayList<Employee> employees = new ArrayList<Employee>();
+		employees = systemApp.getEmployeeObjects();
+		int indexObject = systemApp.searchEmployeeIndex(employees, "1");
+		
+
+		assertEquals(employees.get(indexObject).getFirstName(), employees.get(0).getFirstName());
+
 	}
 	
 }
